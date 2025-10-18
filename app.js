376eaee0c3c3d1f4a7d3ad1aa90b2d785032033c
Neetesh Yadav <neeteshyadav0206@gmail.com>
2025-10-18 13:17:46 +0530

    Prepare project for Render deployment

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ if(process.env.NODE_ENV!="production"){
 require("dotenv").config();
 }
 
-
+const port = process.env.PORT || 8080;
 
 const express=require("express");
 const app=express();
@@ -122,6 +122,6 @@ app.use((err,req,res,next)=>{
 })
 
 
-app.listen(8080,()=>{
+app.listen(port,()=>{
     console.log("server is listening to port 8080");
 });
